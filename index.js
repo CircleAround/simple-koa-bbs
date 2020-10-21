5945da70b9a5365241bbd2ef2ebe9e4ce91a35ec
ms2sato <ms2.sato@gmail.com>
2020-10-22 00:04:33 +0900

    the first

---
+++ index.js
@@ -0,0 +1,38 @@
+const path = require('path');
+const views = require('koa-views');
+const logger = require('koa-logger');
+const router = require('@koa/router')();
+const koaBody = require('koa-body');
+const bodyParser = require('koa-bodyparser');
+
+const Koa = require('koa');
+const app = new Koa();
+app
+  .use(logger())
+  .use(bodyParser())
+  .use(views(path.join(__dirname, '/views'), { extension: 'ejs' }))
+  .use(router.routes())
+  .use(router.allowedMethods())
+  .use(koaBody())
+
+const posts = []
+
+const top = async ctx => {
+  await ctx.render('top', { 
+    test: new Date(), 
+    posts: posts
+  })
+}
+
+const create = async ctx => {
+  const post = ctx.request.body;
+  post.created_at = new Date();
+  posts.push(post)
+  ctx.redirect('/');
+}
+
+router
+  .get('/', top)
+  .post('/post', create);
+
+app.listen(3000);
