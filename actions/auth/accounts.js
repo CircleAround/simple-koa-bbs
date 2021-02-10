a4a447a63e09ad931e99108070d7d313e69c0bb9
ms2sato <ms2.sato@gmail.com>
2021-02-10 23:36:19 +0900

    refactor test mail

--- actions/auth/accounts.js
+++ actions/auth/accounts.js
@@ -1,4 +1,5 @@
 const { ValidationError } = require('sequelize')
+const mail = require('../../lib/mail')
 const db = require('../../models')
 const User = db.user
 
@@ -7,24 +8,39 @@ const renderIndex = async (ctx, params, error = null) => {
 }
 
 const index = async ctx => {
+  sendConfirmationMail({email: 'test@example.com'}) // TODO: This is test
   await renderIndex(ctx, {})
 }
 
 const create = async ctx => {
   const { email, password, nickName } = ctx.request.body
   const params = { email, password, nickName }
-  
+
   try {
     const user = await User.register(params)
+    sendConfirmationMail(user)
     ctx.redirect('/login')
   } catch (e) {
-    if(e instanceof ValidationError) {
+    if (e instanceof ValidationError) {
       console.log(e)
       return await renderIndex(ctx, params, e)
     }
 
     throw e
   }
+}
+
+const sendConfirmationMail = async (user) => {
+  // send mail with defined transport object
+  let info = await mail.mailer().sendMail({
+    from: '"SimpleBBS" <noreply@example.com>', // sender address
+    to: user.email, // list of receivers
+    subject: "[SimpleBBS]ユーザー登録確認", // Subject line
+    text: "Hello world?", // plain text body
+    html: "<b>Hello world?</b>", // html body
+  });
+
+  console.log("Message sent: %s", info.messageId);
 }
 
 module.exports = { index, create }
