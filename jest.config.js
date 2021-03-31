7327f068a84b349a6c929532dc7872a9b391df14
ms2sato <ms2.sato@gmail.com>
2021-03-31 14:43:05 +0900

    fix coverage from

--- jest.config.js
+++ jest.config.js
@@ -20,11 +20,12 @@ module.exports = {
   // collectCoverage: false,
 
   // An array of glob patterns indicating a set of files for which coverage information should be collected
-  // "collectCoverageFrom": [
-  //   "**/*.{js,jsx}",
-  //   "!**/node_modules/**",
-  //   "!**/vendor/**"
-  // ],
+  "collectCoverageFrom": [
+    "app/**/*.{js,jsx}",
+    "extensions/**/*.{js,jsx}",
+    "!**/node_modules/**",
+    "!**/vendor/**"
+  ],
 
   // The directory where Jest should output its coverage files
   // coverageDirectory: "coverage",
