ab2a78d4ab71b37c6500f7305a59225183e90150
ms2sato <ms2.sato@gmail.com>
2021-04-02 11:58:19 +0900

    fix initializer

--- config/initializer.js
+++ config/initializer.js
@@ -2,8 +2,8 @@ const path = require('path')
 
 const { initialize } = require('../lib/autoload')
 
-module.exports = () => {
-  return initialize(
+module.exports = async () => {
+  return await initialize(
     path.join(__dirname, '../extensions'), 
     path.basename(__filename),
     path.join(__dirname, '../config/extensions'), 
